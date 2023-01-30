f373682743e36501c5603f39407ddfc7bddb80f3
jules-jean-louis1 <jules.jean-louis@laplateforme.io>
2023-01-30 18:11:49 +0100

    change new version

--- index.php
+++ index.php
@@ -1,5 +1,15 @@
-<?php
-//'.getcwd().'
-header('location: ./views/home.php');
-// ?>
-<!--  -->
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Auto</title>
+</head>
+<body>
+    <main>
+
+    </main>
+</body>
+</html>
