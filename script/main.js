080c091d85aa11448b8e9d0acfdb8219dfcb4d94
jules-jean-louis1 <jules.jean-louis@laplateforme.io>
2023-02-01 09:59:46 +0100

    resolution probleme reponse url non json car mauvaise requete sql

--- script/main.js
+++ script/main.js
@@ -4,29 +4,28 @@ search.addEventListener("input", getElement);
 async function getElement(event) {
     let query = search.value;
     console.log(query);
-    await fetch('process.php?query='+query)
+    let request = await fetch('process.php?query='+query)
         .then((response) => {
             console.log(response);
-        return response.json();
-    })
-    .then((data) => {
-        let results = document.querySelector("#results");
-        results.innerHTML = "";
-        for (const element of data) {
-            let result = document.createElement("div");
-            result.setAttribute("class", "result");
-            result.innerHTML = element.title;
-            result.addEventListener("click", (event) => {
-                search.value = element.title;
-                results.innerHTML = "";
-            });
-            result.append(result);
-        }
-        if (query == "") {
+            return response.json();
+        })
+        .then((data) => {
+            let results = document.querySelector("#results");
             results.innerHTML = "";
-        }
-    });
-
+            for (const element of data) {
+                let result = document.createElement("div");
+                result.setAttribute("class", "result");
+                result.innerHTML = element.nom;
+                result.addEventListener("click", (event) => {
+                    search.value = element.nom;
+                    results.innerHTML = "";
+                });
+                results.appendChild(result);
+            }
+            if (query == "") {
+                results.innerHTML = "";
+            }
+        });
 }
 
 
