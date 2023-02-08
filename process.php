865ba2895524d9900a2e0a55af37a07fb78dd356
jules-jean-louis1 <jules.jean-louis@laplateforme.io>
2023-02-08 12:10:28 +0100

    ajout commentaire

--- process.php
+++ process.php
@@ -1,4 +1,13 @@
 <?php
+//Ce code PHP traite une requête GET envoyée à ce fichier et implémente une fonction d'autocomplétion en utilisant la base de données.
+//
+//    La première étape vérifie si le paramètre query a été envoyé avec la requête GET en utilisant isset($_GET['query']).
+//
+//    Si query a été envoyé, la variable $query est définie en exécutant une requête SQL à l'aide de la méthode query sur un objet PDO. La requête SELECT récupère tous les enregistrements de la colonne "book" de la table "livre" où le contenu de "book" commence par la valeur envoyée dans "query" et limite le nombre de résultats à 10.
+//
+//    La variable $result est définie en appelant la méthode fetchAll sur $query pour récupérer tous les enregistrements sous forme d'un tableau associatif.
+//
+//    Enfin, la fonction json_encode est appelée pour encoder $result en JSON et l'envoyer en réponse à la requête.
 require "connexion/bddConnect.php";
 // requete pour récupérer les données lorsque l'utilisateur tape dans l'input
 
--- script/main.js
+++ script/main.js
@@ -1,6 +1,8 @@
+//Lorsqu'un événement "input" est déclenché sur l'élément HTML avec l'ID "searchInput", la fonction getElement est appelée.
 const search = document.querySelector("#searchInput");
 
 search.addEventListener("input", getElement);
+//La fonction getElement commence par récupérer la valeur actuelle de l'input en utilisant search.value.
 async function getElement(event) {
     let query = search.value;
     console.log(query);
@@ -27,5 +29,11 @@ async function getElement(event) {
             }
         });
 }
+/*
+Ensuite, la fonction utilise fetch pour envoyer une requête GET à "process.php" avec le paramètre query défini par la valeur actuelle de l'input.
 
+Lorsque la réponse est reçue, elle est parsée en JSON en utilisant response.json() et la boucle for...of itère sur les éléments de la réponse pour créer des éléments "div" avec la classe "result".
 
+    Pour chaque élément "div", son contenu est défini en utilisant element.book et un événement "click" est ajouté pour définir la valeur de l'input sur le contenu lorsqu'il est cliqué.
+
+    Finalement, les éléments "div" sont ajoutés au div avec l'ID "results". Si la valeur de l'input est vide, le contenu de "results" est effacé.*/
